7f43ac6920dc05b1f994731d09140b592398d902
Luis_dev <73004377+Another-DevX@users.noreply.github.com>
2023-12-02 13:20:44 -0500

    feat: add validation

--- kiwi/src/app/page.tsx
+++ kiwi/src/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from 'next/link'
+
 export default function Home () {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <w3m-button />
+      <Link href='/validation' >Validation</Link>
     </main>
   )
 }
