6cc0e62f7d890ccc1a5f9ab8f38c29f20cda6b2b
simon0820s <simon0820s@gmail.com>
2023-12-02 13:18:00 -0500

    create function to fetch flask api

--- kiwi/src/services/validate.ts
+++ kiwi/src/services/validate.ts
@@ -1,3 +1,15 @@
 export default async function validate () {
-   const response = await fetch('')
+  try {
+   const response = await fetch('http://localhost:2809')
+   if (!response.ok) {
+    throw new Error("Error at get response")
+   }
+   const data = await response.json()
+   console.log('Data: ', data)
+   return data;
+  } catch(error) {
+    console.log("Error at fetch validation api", error)
+    return null
+  }
+
 } 
